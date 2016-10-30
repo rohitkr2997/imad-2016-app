28a5977b37b58f5cbb25f7776566eb7214bc1283
rohitkr2997 <rohitkr2997@gmail.com>
2016-10-30 18:31:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,12 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML='New Value';
+
+var image=document.getElementById('madi');
+image.onclick=function(){
+    
+     img.style.marginLeft='100px';  
+    
+    
+    
+};
