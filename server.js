7e3b19ced54209318e50d8aecdb13b97d1fd3837
rohitkr2997 <rohitkr2997@gmail.com>
2016-10-31 19:57:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config ={
+  
+        user: 'rohitkr2997',
+        database: 'rohitkr2997',
+        host : 'db.imad.hasura-app.io',
+        port : '5432',
+        password :process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -80,6 +90,24 @@ app.get('/counter', function(req,res){
     counter=counter+1;
     res.send(counter.toString());
     
+    
+});
+
+
+var pool = new Pool(config);
+
+app.get('/test-db', function(){
+    
+   pool.query('SELECT * FROM test', function(err,result){
+       
+       if(err){
+           res.status(500).send(err.toString());
+       }else{
+           
+           res.send(JSON.stringify(result));
+       }
+       
+   });     
     
 });
 
