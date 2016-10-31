a9508cc2e7ec6c62e235bd3bbdf417ecf115b1a5
rohitkr2997 <rohitkr2997@gmail.com>
2016-10-31 22:15:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.get('/articles/:articleName', function (req, res) {
     
   
     
-    pool.query("SELECT * FROM article WHERE title= '" + req.params.articleName +"'", function(err,result){
+    pool.query("SELECT * FROM article WHERE title= $1" ,[req.params.articleName], function(err,result){
         
         if(err){
             
