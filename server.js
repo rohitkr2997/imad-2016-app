b54a6c5e6c3dabb83dbe1b4728e2966b9c923d35
rohitkr2997 <rohitkr2997@gmail.com>
2016-10-31 21:30:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/counter', function(req,res){
 
 var pool = new Pool(config);
 
-app.get('/test-db', function(){
+app.get('/test-db', function(req,res){
     
    pool.query('SELECT * FROM test', function(err,result){
        
