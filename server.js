bcd7d08ad627defd09ff837f9cfbf93bba1b971f
rohitkr2997 <rohitkr2997@gmail.com>
2016-11-05 15:04:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -150,6 +150,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/BLOGdroid', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'BLOGdroid.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
