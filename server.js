c2a74230d64cc46e233fcd78a0c56ab10e82e53c
rohitkr2997 <rohitkr2997@gmail.com>
2016-11-12 20:30:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.get('/test-db', function(req,res){
 
 app.get('/submit-name',function(req,res){
     
-    var name=req.query.name;
+    var name=req.query.name;   
     
     names.push(name);
     
